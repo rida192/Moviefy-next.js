1fbd9e46f1d1327ede995b4f575cb2c814e91f62
Mortada <rida19974@gmail.com>
2024-05-29 16:29:33 +0300

    CHORE fix actorDetail background

--- components/actorDetails.tsx
+++ components/actorDetails.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const ActorDetails = ({ actor }: { actor: CastMember }) => {
   return (
-    <div className="my-2.5 shadow-[0_5px_10px_0_rgba(0,0,0,0.3)] shadow-black/70 rounded-sm overflow-hidden">
+    <div className="my-2.5 shadow-[0_5px_10px_0_rgba(0,0,0,0.3)] shadow-black/70 rounded-sm overflow-hidden bg-gradient-to-tl dark:from-white/20  dark:to-[#222] hover:from-white/30 hover:saturate-[2.5]  backdrop-blur-lg text-center p-2 relative transition-all duration-100 group  md:hover:scale-[1.03] text-black dark:text-white dark:shadow-none ">
       <div className="h-[150px] sm:h-[200px] ">
         <Image
           className="transition w-full h-full duration-200 object-fill"
-          src={`https://media.themoviedb.org/t/p/w138_and_h175_face${actor?.profile_path}`}
+          src={`https://media.themoviedb.org/t/p/w276_and_h350_face${actor?.profile_path}`}
           // srcSet={`https://media.themoviedb.org/t/p/w138_and_h175_face${actor?.profile_path} 1x, https://media.themoviedb.org/t/p/w276_and_h350_face${actor?.profile_path} 2x`}
           // alt={actor.original_name}
           width={100}
