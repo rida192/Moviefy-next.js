100e58775b63fdc3b0974277ddb6b8b6f40078d7
Mortada <rida19974@gmail.com>
2025-08-17 13:37:23 +0300

    FIX resolve main page conflict

--- app/(root)/movies/page.tsx
+++ app/(root)/movies/page.tsx
@@ -1,9 +1,28 @@
-// app/movies/page.tsx
-export default function MoviesPage() {
-  return (
-    <div className="p-4">
-      <h1>All Movies</h1>
-      <p>This page intentionally left blank.</p>
+import PaginationControls from "@/components/paginationControls";
+import { fetchLatestMovies } from "@/lib/endpoints";
+import MoviesGrid from "@/components/moviesGrid";
+
+export default async function MoviesPage({ searchParams }: SearchParamsProps) {
+  const pageNumber = searchParams["page"] ?? "1";
+
+  let moviesList: MovieApiResponse | undefined;
+  let error: string | undefined;
+
+  try {
+    moviesList = await fetchLatestMovies(pageNumber);
+  } catch (err) {
+    console.error(err);
+    error = "Faild to fetch movies";
+  }
+
+  return error ? (
+    <div className=" w-full text-center h-full grid place-content-center self-center">
+      <h1 className="text-2xl">Something Went Wrong!!</h1>
     </div>
-  );
+  ) : moviesList ? (
+    <main>
+      <MoviesGrid moviesList={moviesList} />
+      <PaginationControls />
+    </main>
+  ) : null;
 }
